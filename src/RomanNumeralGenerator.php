96f0180bfdc519697b3cfc218c8dafe3e6b52a3d
Leo Deng <leo.deng@zerologix.com>
2017-03-16 18:04:10 +1100

    break method down

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -7,6 +7,13 @@ namespace Larowlan\RomanNumeral;
  */
 class RomanNumeralGenerator {
 
+  const PART_MAP = [
+    1 => 'Bit',
+    10 => 'Ten',
+    100 => 'Hundred',
+    1000 => 'Thousand',
+  ];
+
   /**
    * Generates a roman numeral from an integer.
    *
@@ -19,7 +26,45 @@ class RomanNumeralGenerator {
    *   Roman numeral representing the passed integer.
    */
   public function generate(int $number, bool $lowerCase = FALSE) : string {
-    return "Not implemented";
+    // check input
+    if ($number < 1 || $number > 3999) {
+      return "Not support $number yet.";
+    }
+
+    $digit_array = $this->makeDigitArray($number); var_dump($digit_array);
+
+    $roman_number = '';
+
+    foreach ($digit_array as $index => $digit) {
+      $part = self::PART_MAP[pow(10, $index)];
+      $method = "make${part}Part";
+      if (method_exists($this, $method)) {
+        $roman_number = $this->{$method}((int) $digit) . $roman_number;
+      }
+      
+    }
+
+    return $roman_number;
+  }
+
+  private function makeDigitArray(int $number) : array {
+    return array_reverse(str_split((string) $number));
+  }
+
+  private function makeBitPart(int $digit) {
+    return '1';
+  }
+
+  private function makeTenPart(int $digit) {
+    return '2';
+  }
+
+  private function makeHundredPart(int $digit) {
+    return '3';
+  }
+
+  private function makeThousandPart(int $digit) {
+    return '4';
   }
 
 }
